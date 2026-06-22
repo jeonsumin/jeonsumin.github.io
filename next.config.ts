fee0a3cbc5fe21f3557734d961c8e62d483fc8a2
jeonsumin <soomis7@naver.com>
2026-06-22 15:46:29 +0900

    fix: serve GitHub Pages assets from root

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
     output: process.env.NODE_ENV === "production" ? "export" : undefined,
-    basePath: process.env.NODE_ENV === "production" ? "/jeonsumin.github.io" : undefined,
     trailingSlash: process.env.NODE_ENV === "production",
     /* config options here */
     reactStrictMode: false,
