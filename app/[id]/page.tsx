7dca0346306f23836e7e45a6ff86ab932827f35e
jeonsumin <soomis7@naver.com>
2026-06-22 15:31:22 +0900

    commit

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,5 +1,10 @@
-import {fetchPortfolioById} from '@entities/project'
+import {fetchPortfolio, fetchPortfolioById} from '@entities/project'
 import {ProjectDetail} from '@widgets/project-detail'
+
+export async function generateStaticParams() {
+    const projects = await fetchPortfolio()
+    return projects.map((project) => ({id: project.id}))
+}
 
 export default async function Detail({params}: {params: Promise<{id: string}>}) {
     const {id} = await params
